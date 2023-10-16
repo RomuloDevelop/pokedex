b72d9df8998f39bb8e75362477b355ac513e7c3e
RomuloDevelop <romulocg25@gmail.com>
2023-10-15 22:11:18 -0400

    First commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hola mundo</h1>
+      <Button color="danger">Danger</Button>
+    </>
+  );
+}
